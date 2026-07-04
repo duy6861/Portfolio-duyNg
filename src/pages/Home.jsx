f8d3b1fb2a04ab0185072a1cf07db622b60f5c2d
duy6861 <duy6861@gmail.com>
2026-07-04 09:53:55 +0700

    Setup React Router and Main Layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <h1 className="text-5xl font-bold">
+                Home Page
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
